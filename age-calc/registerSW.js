77d5c3b1ad31af906369d93e68cf362545fa2095
zcag <cagdassalur@gmail.com>
2026-03-19 19:19:48 +0300

    Updates

---
+++ age-calc/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/micro-apps/age-calc/sw.js', { scope: '/micro-apps/age-calc/' })})}
---
+++ json-formatter/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/micro-apps/json-formatter/sw.js', { scope: '/micro-apps/json-formatter/' })})}
